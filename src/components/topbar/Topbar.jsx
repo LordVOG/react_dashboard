0c9d83e182f0850263b1e7f5b2abdc6bad30b79e
victorvenk <victorolamy@gmail.com>
2022-07-29 12:25:02 +0100

    Large widget done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import './topbar.css'
-import {NotificationsNone, Language, Settings} from '@mui/icons-material';
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 
 export default function Topbar() {
-	return (
-		<div>
-			<div className="topbar">
-				<div className="topbarWrapper">
-					<div className="topLeft">
-						<span className="logo">venkadmin</span>
-					</div>
-					<div className="topRight">
-						<div className="topbarIconContainer">
-							<NotificationsNone/>
-							<span className="topIconBadge">5</span>
-						</div>
-						<div className="topbarIconContainer">
-							<Language/>
-							<span className="topIconBadge">2</span>
-						</div>
-						<div className="topbarIconContainer">
-							<Settings/>
-						</div>
-						<img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-							alt="Avatar" className="topAvatar" />
-						
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div>
+      <div className="topbar">
+        <div className="topbarWrapper">
+          <div className="topLeft">
+            <span className="logo">venkadmin</span>
+          </div>
+          <div className="topRight">
+            <div className="topbarIconContainer">
+              <NotificationsNone />
+              <span className="topIconBadge">5</span>
+            </div>
+            <div className="topbarIconContainer">
+              <Language />
+              <span className="topIconBadge">2</span>
+            </div>
+            <div className="topbarIconContainer">
+              <Settings />
+            </div>
+            <img
+              src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt="Avatar"
+              className="topAvatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
