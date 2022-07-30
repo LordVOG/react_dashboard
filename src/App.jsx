203aea09814e166312343d4d8f22dba5cd9f0ef6
victorvenk <victorolamy@gmail.com>
2022-07-30 05:37:04 +0100

    React-router-dom resolved after 20 hours

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <BrowserRouter>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/users" element={<UserList />}></Route>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
