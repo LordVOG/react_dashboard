68e2e5fe07f63830e11ca5fd4a0bf2325df4f1a5
victorvenk <victorolamy@gmail.com>
2022-07-30 07:13:06 +0100

    User edit for user page done

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,8 +1,76 @@
-import React from 'react';
-import './UserList.css'
+import React, { useState } from "react";
+import "./UserList.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { DeleteOutline } from "@mui/icons-material";
+import { userRows } from "../../DummyData";
+import { Link } from "react-router-dom";
 
 export default function UserList() {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListUserImg" src={params.row.avatar} alt="" />
+            {params.row.userName}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email Address", width: 200 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      width: 90,
+    },
+
+    {
+      field: "transaction",
+      headerName: "Transaction Volume",
+      width: 140,
+    },
+
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
   return (
-	<div className='UserList'>UserList</div>
-  )
+    <div className="UserList">
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={8}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+  );
 }
